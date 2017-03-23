d7bbdd13b86620a9e1eee70522862a868b9053fb
gdraheim <gdraheim@aservo.com>
2017-03-23 09:07:11 +0100

    update micro-version with date and baseyear-diff

--- files/docker/systemctl.py
+++ files/docker/systemctl.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 __copyright__ = "(C) 2016-2017 Guido U. Draheim, for free usage."
-__version__ = "0.3"
+__version__ = "0.3.1124"
 
 import logging
 logg = logging.getLogger("systemctl")
