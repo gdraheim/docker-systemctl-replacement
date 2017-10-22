fb3cb4339413dfe351c18d7c07170fdc21336e8f
Guido Draheim <guidod@gmx.de>
2017-10-22 11:37:43 +0200

    explain star-options to be '(ignored)'

--- files/docker/systemctl.py
+++ files/docker/systemctl.py
@@ -3067,39 +3067,39 @@ def print_result(result):
 if __name__ == "__main__":
     import optparse
     _o = optparse.OptionParser("%prog [options] command [name...]", 
-        epilog="use 'help' command for more information \n [option* are not implemented]")
+        epilog="use 'help' command for more information")
     _o.add_option("--version", action="store_true",
         help="Show package version")
     _o.add_option("--system", action="store_true",
-        help="Connect to system manager (only possibility!)")
-    # _o.add_option("--user", action="store_true",
-    #     help="Connect to user service manager*")
+        help="Connect to system manager (only possibility)")
+    _o.add_option("--user", action="store_true",
+        help="Connect to user service manager (ignored)")
     # _o.add_option("-H", "--host", metavar="[USER@]HOST",
     #     help="Operate on remote host*")
     # _o.add_option("-M", "--machine", metavar="CONTAINER",
     #     help="Operate on local container*")
     _o.add_option("-t","--type", metavar="TYPE", dest="unit_type", default=_unit_type,
-        help="List units of a particual type*")
+        help="List units of a particual type")
     _o.add_option("--state", metavar="STATE",
-        help="List units with particular LOAD or SUB or ACTIVE state*")
+        help="List units with particular LOAD or SUB or ACTIVE state")
     _o.add_option("-p", "--property", metavar="NAME", dest="unit_property", default=_unit_property,
-        help="Show only properties by this name*")
+        help="Show only properties by this name")
     _o.add_option("-a", "--all", action="store_true", dest="show_all", default=_show_all,
-        help="Show all loaded units/properties, including dead empty ones. To list all units installed on the system, use the 'list-unit-files' command instead*")
+        help="Show all loaded units/properties, including dead empty ones. To list all units installed on the system, use the 'list-unit-files' command instead")
     _o.add_option("-l","--full", action="store_true", default=_full,
-        help="Don't ellipsize unit names on output*")
+        help="Don't ellipsize unit names on output (never ellipsized)")
     _o.add_option("--reverse", action="store_true",
-        help="Show reverse dependencies with 'list-dependencies'*")
+        help="Show reverse dependencies with 'list-dependencies' (ignored)")
     _o.add_option("--job-mode", metavar="MODE",
-        help="Specifiy how to deal with already queued jobs, when queuing a new job*")    
+        help="Specifiy how to deal with already queued jobs, when queuing a new job (ignored)")    
     _o.add_option("--show-types", action="store_true",
-        help="When showing sockets, explicitly show their type*")
+        help="When showing sockets, explicitly show their type (ignored)")
     _o.add_option("-i","--ignore-inhibitors", action="store_true",
-        help="When shutting down or sleeping, ignore inhibitors*")
+        help="When shutting down or sleeping, ignore inhibitors (ignored)")
     _o.add_option("--kill-who", metavar="WHO",
-        help="Who to send signal to*")
+        help="Who to send signal to (ignored)")
     _o.add_option("-s", "--signal", metavar="SIG",
-        help="Which signal to send*")
+        help="Which signal to send (ignored)")
     _o.add_option("--now", action="store_true", default=_now,
         help="Start or stop unit in addition to enabling or disabling it")
     _o.add_option("-q","--quiet", action="store_true", default=_quiet,
@@ -3111,27 +3111,27 @@ if __name__ == "__main__":
     _o.add_option("--no-wall", action="store_true", default=False,
         help="Don't send wall message before halt/power-off/reboot (ignored)")
     _o.add_option("--no-reload", action="store_true",
-        help="Don't reload daemon after en-/dis-abling unit files*")
+        help="Don't reload daemon after en-/dis-abling unit files (ignored)")
     _o.add_option("--no-ask-password", action="store_true", default=_no_ask_password,
         help="Do not ask for system passwords")
     # _o.add_option("--global", action="store_true", dest="globally", default=_globally,
     #    help="Enable/disable unit files globally") # for all user logins
     # _o.add_option("--runtime", action="store_true",
-    #     help="Enable unit files only temporarily until next reboot*")
+    #     help="Enable unit files only temporarily until next reboot")
     _o.add_option("--force", action="store_true", default=_force,
         help="When enabling unit files, override existing symblinks / When shutting down, execute action immediately")
     _o.add_option("--preset-mode", metavar="TYPE", default=_preset_mode,
         help="Apply only enable, only disable, or all presets [%default]")
     _o.add_option("--root", metavar="PATH", default=_root,
-        help="Enable unit files in the specified root directory*")
+        help="Enable unit files in the specified root directory (abused for chroot-like testing)")
     _o.add_option("-n","--lines", metavar="NUM",
-        help="Number of journal entries to show*")
+        help="Number of journal entries to show (ignored)")
     _o.add_option("-o","--output", metavar="CAT",
-        help="change journal output mode (short, ..., cat)*")
+        help="change journal output mode [short, ..., cat] (ignored)")
     _o.add_option("--plain", action="store_true",
-        help="Print unit dependencies as a list instead of a tree*")
+        help="Print unit dependencies as a list instead of a tree (ignored)")
     _o.add_option("--no-pager", action="store_true",
-        help="Do not pipe output into pager*")
+        help="Do not pipe output into pager (ignored)")
     _o.add_option("-v","--verbose", action="count", default=0,
         help="..increase debugging information level")
     _o.add_option("-4","--ipv4", action="store_true", default=False,
