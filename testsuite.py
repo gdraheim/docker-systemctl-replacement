867d6edef462b3d93e38c2c8569f50b902ddcfa2
Guido Draheim <guidod@gmx.de>
2017-12-29 06:32:09 +0100

    adapt to current loggs

--- testsuite.py
+++ testsuite.py
@@ -424,7 +424,7 @@ class DockerSystemctlReplacementTest(unittest.TestCase):
         out, end = output2(cmd.format(**locals()))
         logg.info(" %s =>%s\n%s", cmd, end, out)
         self.assertEqual(end, 0)
-        self.assertEqual(len(greps(open(logfile), " INFO ")), 2)
+        self.assertEqual(len(greps(open(logfile), " INFO ")), 1)
         self.assertEqual(len(greps(open(logfile), " DEBUG ")), 0)
         self.rm_testdir()
         self.coverage()
@@ -440,7 +440,7 @@ class DockerSystemctlReplacementTest(unittest.TestCase):
         out, end = output2(cmd.format(**locals()))
         logg.info(" %s =>%s\n%s", cmd, end, out)
         self.assertEqual(end, 0)
-        self.assertEqual(len(greps(open(logfile), " INFO ")), 2)
+        self.assertEqual(len(greps(open(logfile), " INFO ")), 1)
         self.assertEqual(len(greps(open(logfile), " DEBUG ")), 3)
         self.rm_testdir()
         self.coverage()
@@ -839,7 +839,6 @@ class DockerSystemctlReplacementTest(unittest.TestCase):
         self.assertTrue(greps(out, r"d.service: Service lacks both ExecStart and ExecStop"))
         self.assertTrue(greps(out, r"g.service: there may be only one ExecStart statement"))
         self.assertTrue(greps(out, r"g.service: there may be only one ExecStop statement"))
-        self.assertTrue(greps(out, r"g.service: there may be only one ExecReload statement"))
         self.assertTrue(greps(out, r"c.service: the use of /bin/kill is not recommended"))
     def real_1090_syntax_errors_are_shown_in_journal_after_try_start(self):
         """ check that preset files do work internally"""
@@ -3760,6 +3759,176 @@ class DockerSystemctlReplacementTest(unittest.TestCase):
         actZX, exitZX  = output2(is_active_ZX.format(**locals()))
         self.assertEqual(actZX.split("\n"), ["unknown", ""])
         self.assertEqual(exitZX, 1)
+        #
+        kill_testsleep = "killall {testsleep}"
+        sx____(kill_testsleep.format(**locals()))
+        self.rm_testdir()
+        sx____("rm /var/run/zz*")
+        sx____("rm /etc/systemd/system/zz*")
+        sx____("systemctl daemon-reload")
+    def test_3063_is_active_for_forking_delayed(self):
+        """ check that we can start forking services and have them is-active,
+            even when the pid-file is created later because startup waits
+            for its existance."""
+        vv = "-vv"
+        testname = self.testname()
+        testdir = self.testdir()
+        user = self.user()
+        root = self.root(testdir)
+        systemctl = _cov + _systemctl_py + " --root=" + root
+        testsleep = self.testname("sleep")
+        bindir = os_path(root, "/usr/bin")
+        os.makedirs(os_path(root, "/var/run"))
+        shell_file(os_path(testdir, "zzz.init"), """
+            #! /bin/bash
+            case "$1" in start) 
+               [ -d /var/run ] || mkdir -p /var/run
+               ({bindir}/{testsleep} 50 0<&- &>/dev/null &
+                sleep 4
+                echo $! > {root}/var/run/zzz.init.pid
+               ) &
+               sleep 1
+               ps -o pid,ppid,args
+            ;; stop)
+               killall {testsleep}
+            ;; esac 
+            echo "done$1" >&2
+            exit 0
+            """.format(**locals()))
+        text_file(os_path(testdir, "zzz.service"),"""
+            [Unit]
+            Description=Testing Z
+            [Service]
+            Type=forking
+            PIDFile={root}/var/run/zzz.init.pid
+            ExecStart={root}/usr/bin/zzz.init start
+            ExecStop={root}/usr/bin/zzz.init stop
+            [Install]
+            WantedBy=multi-user.target
+            """.format(**locals()))
+        copy_tool("/usr/bin/sleep", os_path(bindir, testsleep))
+        copy_tool(os_path(testdir, "zzz.init"), os_path(root, "/usr/bin/zzz.init"))
+        copy_file(os_path(testdir, "zzz.service"), os_path(root, "/etc/systemd/system/zzz.service"))
+        #
+        is_active_ZX = "{systemctl} is-active zzz.service {vv}"
+        actZX, exitZX  = output2(is_active_ZX.format(**locals()))
+        self.assertEqual(actZX.split("\n"), ["inactive", ""])
+        self.assertEqual(exitZX, 3)
+        #
+        cmd = "{systemctl} start zzz.service -vv"
+        out, end = output2(cmd.format(**locals()))
+        logg.info(" %s =>%s\n%s", cmd, end, out)
+        self.assertEqual(end, 0)
+        top = output(_top_recent)
+        logg.info("\n>>>\n%s", top)
+        self.assertTrue(greps(top, testsleep))
+        #
+        is_active_ZX = "{systemctl} is-active zzz.service {vv}"
+        actZX, exitZX  = output2(is_active_ZX.format(**locals()))
+        self.assertEqual(actZX.split("\n"), ["active", ""])
+        self.assertEqual(exitZX, 0)
+        self.assertTrue(os.path.exists(os_path(root, "/var/run/zzz.init.pid")))
+        time.sleep(4)
+        self.assertTrue(os.path.exists(os_path(root, "/var/run/zzz.init.pid")))
+        #
+        cmd = "{systemctl} stop zzz.service -vv"
+        out, end = output2(cmd.format(**locals()))
+        logg.info(" %s =>%s\n%s", cmd, end, out)
+        self.assertEqual(end, 0)
+        top = output(_top_recent)
+        logg.info("\n>>>\n%s", top)
+        self.assertFalse(greps(top, testsleep))
+        #
+        is_active_ZX = "{systemctl} is-active zzz.service {vv}"
+        actZX, exitZX  = output2(is_active_ZX.format(**locals()))
+        self.assertEqual(actZX.split("\n"), ["inactive", ""])
+        self.assertEqual(exitZX, 3)
+        #
+        kill_testsleep = "killall {testsleep}"
+        sx____(kill_testsleep.format(**locals()))
+        self.rm_testdir()
+        self.coverage()
+    def real_3063_is_active_for_forking_delayed(self):
+        """ check that we can start forking services and have them is-active
+            even when the pid-file is created later."""
+        # TODO: test_3063 does differ from real_3063
+        vv = ""
+        testname = self.testname()
+        testdir = self.testdir()
+        user = self.user()
+        root = ""
+        systemctl = "/usr/bin/systemctl"
+        testsleep = self.testname("sleep")
+        bindir = os.path.realpath(os_path(testdir, "/usr/bin"))
+        shell_file(os_path(testdir, "zzz.init"), """
+            #! /bin/bash
+            case "$1" in start) 
+               [ -d /var/run ] || mkdir -p /var/run
+               ({bindir}/{testsleep} 50 0<&- &>/dev/null &
+                sleep 4
+                echo $! > {root}/var/run/zzz.init.pid
+               ) &
+               sleep 1
+               ps -o pid,ppid,args
+            ;; stop)
+               killall {testsleep}
+            ;; esac 
+            echo "done$1" >&2
+            exit 0
+            """.format(**locals()))
+        text_file(os_path(testdir, "zzz.service"),"""
+            [Unit]
+            Description=Testing Z
+            [Service]
+            Type=forking
+            PIDFile={root}/var/run/zzz.init.pid
+            ExecStart={root}/usr/bin/zzz.init start
+            ExecStop={root}/usr/bin/zzz.init stop
+            [Install]
+            WantedBy=multi-user.target
+            """.format(**locals()))
+        copy_tool("/usr/bin/sleep", os_path(bindir, testsleep))
+        copy_tool(os_path(testdir, "zzz.init"), os_path(root, "/usr/bin/zzz.init"))
+        copy_file(os_path(testdir, "zzz.service"), os_path(root, "/etc/systemd/system/zzz.service"))
+        sh____("systemctl daemon-reload")
+        sx____("systemctl kill zzz.service")
+        sx____("systemctl stop zzz.service")
+        sh____("systemctl daemon-reload")
+        # sh____("systemctl enable zzz.service")
+        #
+        is_active_ZX = "{systemctl} is-active zzz.service {vv}"
+        actZX, exitZX  = output2(is_active_ZX.format(**locals()))
+        self.assertEqual(actZX.split("\n"), ["unknown", ""])
+        self.assertEqual(exitZX, 3)
+        #
+        cmd = "{systemctl} start zzz.service {vv}"
+        out, end = output2(cmd.format(**locals()))
+        logg.info(" %s =>%s\n%s", cmd, end, out)
+        self.assertEqual(end, 0)
+        top = output(_top_recent)
+        logg.info("\n>>>\n%s", top)
+        self.assertTrue(greps(top, testsleep))
+        #
+        is_active_ZX = "{systemctl} is-active zzz.service {vv}"
+        actZX, exitZX  = output2(is_active_ZX.format(**locals()))
+        self.assertEqual(actZX.split("\n"), ["active", ""])
+        self.assertEqual(exitZX, 0)
+        self.assertTrue(os.path.exists(os_path(root, "/var/run/zzz.init.pid")))
+        time.sleep(4)
+        self.assertTrue(os.path.exists(os_path(root, "/var/run/zzz.init.pid")))
+        #
+        cmd = "{systemctl} stop zzz.service {vv}"
+        out, end = output2(cmd.format(**locals()))
+        logg.info(" %s =>%s\n%s", cmd, end, out)
+        self.assertEqual(end, 0)
+        top = output(_top_recent)
+        logg.info("\n>>>\n%s", top)
+        self.assertFalse(greps(top, testsleep))
+        #
+        is_active_ZX = "{systemctl} is-active zzz.service {vv}"
+        actZX, exitZX  = output2(is_active_ZX.format(**locals()))
+        self.assertEqual(actZX.split("\n"), ["unknown", ""])
+        self.assertEqual(exitZX, 3)
         #
         kill_testsleep = "killall {testsleep}"
         sx____(kill_testsleep.format(**locals()))
@@ -10759,7 +10928,7 @@ class DockerSystemctlReplacementTest(unittest.TestCase):
         self.assertTrue(greps(open(testdir+"/systemctl.debug.log"), "notify start done"))
         self.assertTrue(greps(open(testdir+"/systemctl.debug.log"), "stop '/bin/kill' '-WINCH'"))
         self.assertTrue(greps(open(testdir+"/systemctl.debug.log"), "wait [$]NOTIFY_SOCKET"))
-        self.assertTrue(greps(open(testdir+"/systemctl.debug.log"), "(dead)"))
+        self.assertTrue(greps(open(testdir+"/systemctl.debug.log"), "wait for PID .* is done"))
         self.rm_testdir()
     def test_7012_centos_elasticsearch(self):
         """ WHEN we can setup a specific ElasticSearch version 
@@ -11292,8 +11461,8 @@ class DockerSystemctlReplacementTest(unittest.TestCase):
         sh____(read_logfile_2.format(**locals()))
         self.assertFalse(greps(open(tmp+"/systemctl.server.log"), " ERROR "))
         self.assertFalse(greps(open(tmp+"/systemctl.desktop.log"), " ERROR "))
-        self.assertGreater(len(greps(open(tmp+"/systemctl.server.log"), " INFO ")), 10)
-        self.assertGreater(len(greps(open(tmp+"/systemctl.desktop.log"), " INFO ")), 10)
+        self.assertGreater(len(greps(open(tmp+"/systemctl.server.log"), " INFO ")), 6)
+        self.assertGreater(len(greps(open(tmp+"/systemctl.desktop.log"), " INFO ")), 6)
         self.assertTrue(greps(open(tmp+"/systemctl.server.log"), "/systemctl daemon-reload"))
         # self.assertTrue(greps(open(tmp+"/systemctl.server.log"), "/systemctl status jenkins.service"))
         # self.assertTrue(greps(open(tmp+"/systemctl.server.log"), "--property=ActiveState")) # <<< new
