4c09b1831549d3f4efbe6c136386e4519adde45e
Guido Draheim <guidod@gmx.de>
2018-08-30 15:47:03 +0200

    switch to newer docker naming scheme with host:port/project/product:version

--- testsuite.py
+++ testsuite.py
@@ -43,7 +43,7 @@ TESTED = [ "centos:7.3.1611", "centos:7.4.1708", "centos:7.5.1804" ]
 TESTED += [ "opensuse:42.2", "opensuse:42.3", "opensuse/leap:15.0" ]
 TESTED += [ "ubuntu:14.04" ]
 
-IMAGES = "localhost:5000/testingsystemctl"
+IMAGES = "localhost:5000/systemctl/testing"
 CENTOS = "centos:7.4.1708"
 UBUNTU = "ubuntu:14.04"
 OPENSUSE = "opensuse:42.3"
