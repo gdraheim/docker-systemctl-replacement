71892b841800d4a3e8ae7e45523e842b7b1aaef4
Guido Draheim <guidod@gmx.de>
2018-09-05 05:39:09 +0200

    v1.4.2363

--- testsuite.py
+++ testsuite.py
@@ -4,7 +4,7 @@
 from __future__ import print_function
 
 __copyright__ = "(C) Guido Draheim, licensed under the EUPL"""
-__version__ = "1.4.2362"
+__version__ = "1.4.2363"
 
 ## NOTE:
 ## The testcases 1000...4999 are using a --root=subdir environment
