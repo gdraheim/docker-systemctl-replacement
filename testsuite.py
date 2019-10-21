f61f389c40855a59c3a88cc90e465384ca90948a
Guido Draheim <guidod@gmx.de>
2019-10-21 20:15:50 +0200

    real test and NOT_FOUND on mask_modules

--- testsuite.py
+++ testsuite.py
@@ -8042,20 +8042,30 @@ class DockerSystemctlReplacementTest(unittest.TestCase):
         self.rm_testdir()
         self.coverage()
         self.end()
-    def test_3811_mask_some_unknown(self):
+    def real_3811_mask_some_unknown(self):
+        self. test_3811_mask_some_unknown(True)
+    def test_3811_mask_some_unknown(self, real = None):
         """ check mask some unknown unit fails okay"""
-        self.begin()
+        vv = self.begin()
         testname = self.testname()
         testdir = self.testdir()
-        root = self.root(testdir)
+        root = self.root(testdir, real)
         systemctl = cover() + _systemctl_py + " --root=" + root
+        if real: vv, systemctl = "", "/usr/bin/systemctl"
         #
-        cmd = "{systemctl} mask zz-unknown.service -vv"
+        sh____("{systemctl} daemon-reload".format(**locals()))
+        cmd = "{systemctl} mask zz-unknown.service {vv}"
         out, err, end = output3(cmd.format(**locals()))
         logg.info(" %s =>%s \n%s\n%s", cmd, end, err, out)
-        self.assertEqual(end, 1)
-        self.assertTrue(greps(err, "Unit zz-unknown.service could not be found."))
+        if real:
+            self.assertEqual(end, 0)
+            self.assertTrue(greps(err, "Unit zz-unknown.service does not exist, proceeding anyway.")) #TODO
+            self.assertTrue(greps(err, "Created symlink /etc/systemd/system/zz-unknown.service .* /dev/null"))
+        else:
+            self.assertEqual(end, 5)
+            self.assertTrue(greps(err, "Unit zz-unknown.service not found."))
         #
+        self.rm_zzfiles(root)
         self.rm_testdir()
         self.coverage()
         self.end()
