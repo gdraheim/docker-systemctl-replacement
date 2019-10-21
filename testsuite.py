defdd1fb230324baa211990a07b80e2a85de082e
Guido Draheim <guidod@gmx.de>
2019-10-21 19:48:14 +0200

    real_3803_restart_some_unknown and NOT_FOUND on restart_modules

--- testsuite.py
+++ testsuite.py
@@ -7878,19 +7878,23 @@ class DockerSystemctlReplacementTest(unittest.TestCase):
         self.rm_testdir()
         self.coverage()
         self.end()
-    def test_3803_restart_some_unknown(self):
+    def real_3803_restart_some_unknown(self):
+        self. test_3803_restart_some_unknown(True)
+    def test_3803_restart_some_unknown(self, real = None):
         """ check restart some unknown unit fails okay"""
-        self.begin()
+        vv = self.begin()
         testname = self.testname()
         testdir = self.testdir()
-        root = self.root(testdir)
+        root = self.root(testdir, real)
         systemctl = cover() + _systemctl_py + " --root=" + root
+        if real: vv, systemctl = "", "/usr/bin/systemctl"
         #
-        cmd = "{systemctl} restart zz-unknown.service -vv"
+        sh____("{systemctl} daemon-reload".format(**locals()))
+        cmd = "{systemctl} restart zz-unknown.service {vv}"
         out, err, end = output3(cmd.format(**locals()))
         logg.info(" %s =>%s \n%s\n%s", cmd, end, err, out)
-        self.assertEqual(end, 1)
-        self.assertTrue(greps(err, "Unit zz-unknown.service could not be found."))
+        self.assertEqual(end, 5)
+        self.assertTrue(greps(err, "Unit zz-unknown.service not found."))
         #
         self.rm_testdir()
         self.coverage()
