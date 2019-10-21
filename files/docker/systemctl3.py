3f41d9fc81f70440463ff8450b686a60164d39df
Guido Draheim <guidod@gmx.de>
2019-10-21 19:57:41 +0200

    real test and NOT_FOUND on reload_or_restart_modules

--- files/docker/systemctl3.py
+++ files/docker/systemctl3.py
@@ -2566,7 +2566,8 @@ class Systemctl:
         for module in modules:
             matched = self.match_units([ module ])
             if not matched:
-                logg.error("Unit %s could not be found.", unit_of(module))
+                logg.error("Unit %s not found.", unit_of(module))
+                self.error |= NOT_FOUND
                 found_all = False
                 continue
             for unit in matched:
@@ -2585,7 +2586,7 @@ class Systemctl:
         """ do 'reload' if specified, otherwise do 'restart' """
         conf = self.load_unit_conf(unit)
         if conf is None:
-            logg.error("Unit %s could not be found.", unit)
+            logg.error("Unit %s not found.", unit)
             return False
         if self.not_user_conf(conf):
             logg.error("Unit %s not for --user mode", unit)
--- testsuite.py
+++ testsuite.py
@@ -7924,20 +7924,25 @@ class DockerSystemctlReplacementTest(unittest.TestCase):
         self.rm_testdir()
         self.coverage()
         self.end()
-    def test_3805_reload_or_restart_some_unknown(self):
+    def real_3805_reload_or_restart_some_unknown(self):
+        self. test_3805_reload_or_restart_some_unknown(True)
+    def test_3805_reload_or_restart_some_unknown(self, real = None):
         """ check reload-or-restart some unknown unit fails okay"""
-        self.begin()
+        vv = self.begin()
         testname = self.testname()
         testdir = self.testdir()
-        root = self.root(testdir)
+        root = self.root(testdir, real)
         systemctl = cover() + _systemctl_py + " --root=" + root
+        if real: vv, systemctl = "", "/usr/bin/systemctl"
         #
-        cmd = "{systemctl} reload-or-restart zz-unknown.service -vv"
+        sh____("{systemctl} daemon-reload".format(**locals()))
+        cmd = "{systemctl} reload-or-restart zz-unknown.service {vv}"
         out, err, end = output3(cmd.format(**locals()))
         logg.info(" %s =>%s \n%s\n%s", cmd, end, err, out)
-        self.assertEqual(end, 1)
-        self.assertTrue(greps(err, "Unit zz-unknown.service could not be found."))
+        self.assertEqual(end, 5)
+        self.assertTrue(greps(err, "Unit zz-unknown.service not found."))
         #
+        self.rm_zzfiles(root)
         self.rm_testdir()
         self.coverage()
         self.end()
