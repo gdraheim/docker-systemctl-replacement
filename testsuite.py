58e848155817309b39f15fb7b95cef0cc8eee225
Guido Draheim <guidod@gmx.de>
2019-10-23 13:42:12 +0200

    deeper old path on test_4051

--- testsuite.py
+++ testsuite.py
@@ -12634,7 +12634,7 @@ class DockerSystemctlReplacementTest(unittest.TestCase):
         #
         log = open(debug_log).read()
         logg.info("debug.log>>%s", i2(log))
-        self.assertTrue(greps(log, "old notify socketfile [(]1\\d\\d[)]"))
+        self.assertTrue(greps(log, "old notify socketfile [(][12]\\d\\d[)]"))
         self.assertTrue(greps(log, "new notify socketfile [(]99[)]"))
         #
         top = _recent(output(_top_list))
