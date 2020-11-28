bc52dabc0e4027410fd2442bafd75527fdc16032
Guido Draheim <guidod@gmx.de>
2020-11-28 06:22:09 +0100

    v1.5.4475

--- files/docker/systemctl3.py
+++ files/docker/systemctl3.py
@@ -4,7 +4,7 @@
 from __future__ import print_function
 
 __copyright__ = "(C) 2016-2020 Guido U. Draheim, licensed under the EUPL"
-__version__ = "1.5.4431"
+__version__ = "1.5.4475"
 
 import logging
 logg = logging.getLogger("systemctl")
