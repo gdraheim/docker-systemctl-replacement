9e05a2f3f9bca324182c2bd4a2ae1b9c2863d86a
Guido Draheim <guidod@gmx.de>
2020-12-19 17:01:13 +0100

    use 'fmt' instead of 'format' on optional loggers

--- files/docker/systemctl3.py
+++ files/docker/systemctl3.py
@@ -42,12 +42,12 @@ DEBUG_FLOCK = False
 TestListen = False
 TestAccept = False
 
-def logg_debug_flock(format, *args):
+def logg_debug_flock(msg, *args):
     if DEBUG_FLOCK:
-        logg.debug(format, *args) # pragma: no cover
-def logg_debug_after(format, *args):
+        logg.debug(msg, *args) # pragma: no cover
+def logg_debug_after(msg, *args):
     if DEBUG_AFTER:
-        logg.debug(format, *args) # pragma: no cover
+        logg.debug(msg, *args) # pragma: no cover
 
 NOT_A_PROBLEM = 0   # FOUND_OK
 NOT_OK = 1          # FOUND_ERROR
