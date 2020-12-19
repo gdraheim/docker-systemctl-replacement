ec60d256403d1193443cb190801d1b9edab10f59
Guido Draheim <guidod@gmx.de>
2020-12-19 17:57:33 +0100

    allow to update loggers with f-format - single argument

--- format3.py
+++ format3.py
@@ -8,28 +8,37 @@ logg = logging.getLogger("FORMAT")
 logging.addLevelName(DONE, "DONE")
 
 WRITEBACK=False
+COMPAT=False
 
 def run(filename):
     changes = 0
     lines = []
-    linetext = open(filename).read()
-    for line in linetext.splitlines():
-        newline = line
+    srctext = open(filename).read()
+    for line0 in srctext.splitlines():
+        line = line0
         if re.match("^\\s*#", line):
             lines.append(line)
             continue
+        m = re.match(r'^(\s*)(\w+[.]\w+[(])(["][^"]*["]),\s*(\w+)\s*([)].*)$', line)
+        if m:
+            prefix, loggfu, fmt, a, suffix = m.groups()
+            if loggfu in ("logg.debug(", "logg.info(", "logg.warning(", "logg.error("):
+                if not a.startswith("_") and a.lower() == a:
+                    fmt2 = fmt.replace("%s", "{"+a+"}")
+                    if fmt != fmt2:
+                        line = prefix+loggfu+fmt2+".format(**locals())"+suffix
         m = re.match('(^[^"]*)(["][^"]*["])([^"]*)$', line)
         if m:
             prefix, string, suffix = m.groups()
             flocals = ".format(**locals())"
-            if suffix.startswith(flocals):
+            if suffix.startswith(flocals) and not COMPAT:
                 if not prefix.strip().endswith("+"):
-                    newline = prefix+"f"+string+suffix[len(flocals):]
-        if line != newline:
-            logg.info(" -%s", line)
-            logg.info(" +%s", newline)
+                    line = prefix+"f"+string+suffix[len(flocals):]
+        if line != line0:
+            logg.info(" -%s", line0)
+            logg.info(" +%s", line)
             changes += 1
-        lines.append(newline)
+        lines.append(line)
     logg.debug("found %s lines in %s", len(lines), filename)
     if changes:
         logg.log(DONE, "found %s changes for %s", changes, filename)
@@ -45,11 +54,14 @@ if __name__ == "__main__":
                    help="increase logging level [%(default)s]")
     o.add_argument("-i", "--in-place", dest="writeback", action="store_true", default=WRITEBACK,
                    help="write the changes back to the file [%(default)s]")
+    o.add_argument("-2", "--compat", action="store_true", default=COMPAT,
+                   help="update only with python2 compatible parts [%(default)s]")
     o.add_argument("filename", 
                    help="the file to show the changes")
     opt = o.parse_args()
     logging.basicConfig(level = max(0, DONE - 10 * opt.verbose))
     WRITEBACK = opt.writeback
+    COMPAT = opt.compat
     run(opt.filename)
     
     
