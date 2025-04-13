ba3505807fd03f70f352e696e966a0da04dfc1e1
Guido Draheim <guidod@gmx.de>
2025-04-13 13:46:15 +0200

    rework and install journalctl3

--- src/journalctl3.py
+++ src/journalctl3.py
@@ -1,31 +1,46 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3
+# pylint: disable=line-too-long,missing-function-docstring,missing-module-docstring
+# type: ignore
 
 import argparse
-import os
 import sys
+import logging
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--unit', metavar='unit', type=str, required=True, help='Systemd unit to display')
-parser.add_argument('-f', '--follow', default=False, action='store_true', help='Follows the log')
-parser.add_argument('-n', '--lines', metavar='num', type=int, help='Num of lines to display')
-parser.add_argument('--no-pager', default=False, action='store_true', help='Do not pipe through a pager')
-parser.add_argument('--system', default=False, action='store_true', help='Show system units')
-parser.add_argument('--user', default=False, action='store_true', help='Show user units')
-parser.add_argument('--root', metavar='path', type=str, help='Use subdirectory path')
-parser.add_argument('-x', default=False, action='store_true', help='Switch on verbose mode')
-args = parser.parse_args()
 
-systemctl_py = "systemctl3.py"
-path = os.path.dirname(sys.argv[0])
-systemctl = os.path.join(path, systemctl_py)
+try:
+    from . import systemctl3
+except ImportError:
+    import systemctl3
 
-cmd = [ systemctl, "log", args.unit ] # drops the -u
-if args.follow: cmd += [ "-f" ]
-if args.lines: cmd += [ "-n", str(args.lines) ]
-if args.no_pager: cmd += [ "--no-pager" ]
-if args.system: cmd += [ "--system" ]
-elif args.user: cmd += [ "--user" ]
-if args.root: cmd += [ "--root", start(args.root) ]
-if args.x: cmd += [ "-vvv" ]
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--unit', metavar='unit', type=str, required=True, help='Systemd unit to display')
+    parser.add_argument('-f', '--follow', default=False, action='store_true', help='Follows the log')
+    parser.add_argument('-n', '--lines', metavar='num', type=int, help='Num of lines to display')
+    parser.add_argument('--no-pager', default=False, action='store_true', help='Do not pipe through a pager')
+    parser.add_argument('--system', default=False, action='store_true', help='Show system units')
+    parser.add_argument('--user', default=False, action='store_true', help='Show user units')
+    parser.add_argument('--root', metavar='path', type=str, help='Use subdirectory path')
+    parser.add_argument('-x', default=False, action='store_true', help='Switch on verbose mode')
+    args = parser.parse_args()
 
-os.execvp(cmd[0], cmd)
+    if args.follow:
+        systemctl3.DO_FORCE = True
+    if args.lines:
+        systemctl3.LOG_LINES = args.lines
+    if args.no_pager:
+        systemctl3.NO_PAGER = True
+    if args.system:
+        systemctl3.USER_MODE = False
+    elif args.user:
+        systemctl3.USER_MODE = True
+    if args.root:
+        systemctl3.ROOT = args.root
+    if args.x:
+        logging.basicConfig(level = logging.DEBUG)
+    else:
+        logging.basicConfig(level = logging.WARNING)
+
+    return systemctl3.runcommand("log", args.unit)
+
+sys.exit(main())
